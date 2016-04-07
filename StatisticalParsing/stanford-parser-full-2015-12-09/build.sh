8988366af18e84b0653cce07c9ac01ab0e37cbba
Kunal Lad <kunallad423@gmail.com>
2016-04-06 22:49:58 -0500

    Adding README and more condor scripts

--- StatisticalParsing/stanford-parser-full-2015-12-09/build.sh
+++ StatisticalParsing/stanford-parser-full-2015-12-09/build.sh
@@ -1,6 +1,7 @@
 javac -cp "stanford-parser.jar:slf4j-api.jar:." UnsupervisedDomainAdaptationDemo.java
 
-java -cp "stanford-parser.jar:slf4j-api.jar:." UnsupervisedDomainAdaptationDemo \
-  ~/Workspace/NLP/StatisticalParsing/stanford-parser-full-2015-12-09/data/wsjTrain 1000 \
-  ~/Workspace/NLP/StatisticalParsing/stanford-parser-full-2015-12-09/data/brown 1000 \
-  ~/Workspace/NLP/StatisticalParsing/stanford-parser-full-2015-12-09/data/brown
+# Sample command to run for particular arguments.
+# java -cp "stanford-parser.jar:slf4j-api.jar:." UnsupervisedDomainAdaptationDemo \
+#  ~/Workspace/NLP/StatisticalParsing/stanford-parser-full-2015-12-09/data/wsjTrain 1000 \
+#  ~/Workspace/NLP/StatisticalParsing/stanford-parser-full-2015-12-09/data/brown 1000 \
+#  ~/Workspace/NLP/StatisticalParsing/stanford-parser-full-2015-12-09/data/brown
